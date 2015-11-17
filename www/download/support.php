4f926c9d971123ba87f628e21beed959931e5881
Jørn Åne <jorn.dejong@uninett.no>
2015-11-17 15:22:34 +0100

    Set correct css class for logo

--- www/download/support.php
+++ www/download/support.php
@@ -56,7 +56,7 @@ function url($url) {
 ?>
 <h1><?= o($idp->getDisplay()) ?></h1>
 <?php if (!is_null($idp->getIconID())) { ?>
-<p><img src="<?= o($idp->getIconUrl()) ?>" alt="<?= o($idp->getDisplay()) ?> cat-idp-logo">
+<p><img src="<?= o($idp->getIconUrl()) ?>" alt="<?= o($idp->getDisplay()) ?> logo" class="cat-idp-logo">
 <?php } ?>
 <p><?= o($profile->getDescription()) ?>
 
--- www/profiles/support.php
+++ www/profiles/support.php
@@ -1,4 +1,4 @@
 <h1><?= o($idp->getDisplay()) ?></h1>
 <?php if (!is_null($idp->getIconID())) { ?>
-<p><img src="<?= o($idp->getIconUrl()) ?>" alt="<?= o($idp->getDisplay()) ?> logo" style="max-width:100%">
+<p><img src="<?= o($idp->getIconUrl()) ?>" alt="<?= o($idp->getDisplay()) ?> logo" class="cat-idp-logo">
 <?php } ?>
